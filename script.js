82e52c20d4f19962be4da0b1bc03167a08e10d7d
Daniel W Cross <danielwincross94@gmail.com>
2022-09-24 09:33:25 +0700

    update themes. Add initial JS functions to change themes when clickd.

---
+++ script.js
@@ -0,0 +1,37 @@
+// console.log('its working');
+
+// EVENT HANDLERS
+
+let themeDots = document.getElementsByClassName('theme-dot')
+
+
+
+// LOOP THROUGH THEME DOTS
+for(var i=0; themeDots.length > 0; i++){
+    themeDots[i].addEventListener('click', function(){
+        let mode = this.dataset.mode
+        console.log('Option clicked', mode);
+        setTheme(mode)
+    })
+}
+
+
+// FUNCTION SET THEME
+
+function setTheme(mode){
+    if(mode === 'light'){
+        document.getElementById('theme-style').href = 'default.css'
+    }
+
+    if(mode === 'blue'){
+        document.getElementById('theme-style').href = 'blue.css'
+    }
+
+    if(mode === 'green'){
+        document.getElementById('theme-style').href = 'green.css'
+    }
+
+    if(mode === 'purple'){
+        document.getElementById('theme-style').href = 'purple.css'
+    }
+}
